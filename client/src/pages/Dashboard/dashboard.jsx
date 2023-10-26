bb3bb37c16fe7cafa899a8c401fa5ca79ac5a167
JamieRoche1997 <144720306+JamieRoche1997@users.noreply.github.com>
2023-10-26 23:26:24 +0100

    Updated the Dashboard Page with New Patient Data Graphs for Lung Cancer Predictions
    In this update, we have extended the functionality of the Dashboard page to incorporate newly collected patient data. These data-driven enhancements are crucial for our ongoing efforts in lung cancer predictions using machine learning models. We have meticulously curated and integrated patient data that is essential for improving the accuracy and comprehensiveness of our predictions.
    
    Key Updates:
    
    Graphical Insights: We have introduced a range of new graphs, each offering unique insights into patient data. These visualizations are designed to assist medical professionals in identifying patterns and risk factors associated with lung cancer.
    
    Continual Improvement: Our commitment to providing the best predictive tools extends beyond lung cancer. While we have focused on this specific domain in this update, we are actively working on finalizing datasets for colon cancer and heart disease. Once completed, these datasets will serve as valuable resources for creating predictive models and further enhancing the Dashboard's capabilities.
    
    User-Friendly Interface: We understand the importance of presenting data in a clear and user-friendly manner. The new graphs are integrated seamlessly into the Dashboard, ensuring that healthcare providers can easily access and interpret the data.

--- client/src/pages/Dashboard/dashboard.jsx
+++ client/src/pages/Dashboard/dashboard.jsx
@@ -8,14 +8,34 @@ const Dashboard = () => {
   const { user } = useAuthentication();
   const [patients, setPatients] = useState([]);
   const canvasRef = useRef(null);
+  const lifestyleFactorsChartRef = useRef(null);
+  const respiratorySymptomsChartRef = useRef(null);
+  const environmentalFactorsChartRef = useRef(null);
+  const snoringChartRef = useRef(null);
+  const dietDonutChartRef = useRef(null);
   const ageChartRef = useRef(null);
   const genderChartRef = useRef(null);
 
 
-  const [summary, setSummary] = useState({
+  const [summary1, setSummary1] = useState({
     patientCount: 0,
     averageAge: 0,
     mostCommonCondition: '',
+  });
+
+  const [summary, setSummary] = useState({
+    airPollutionCount: 0,
+    dustExposureCount: 0,
+    geneticRiskCount: 0,
+    alcoholConsumptionCount: 0,
+    smokerCount: 0,
+    dietComplianceCount: 0,
+    coughingBloodCount: 0,
+    fatigueCount: 0,
+    wheezingCount: 0,
+    obesityCount: 0,
+    clubbingNailsCount: 0,
+    snoringCount: 0,
   });
 
 
@@ -37,10 +57,8 @@ const Dashboard = () => {
           console.error('Error getting user data from Firestore:', error);
         });
 
-
       // Fetch patient data
       const patientsRef = db.collection('Patient').where('doctor', '==', user.displayName);
-
 
       patientsRef
         .get()
@@ -64,7 +82,6 @@ const Dashboard = () => {
           console.log(patientCount);
           const averageAge = Math.round(totalAge / patientCount);
 
-
           const conditions = patientData.map((patient) => patient.condition);
           const conditionCounts = {};
           let mostCommonCondition = '';
@@ -84,7 +101,7 @@ const Dashboard = () => {
           });
 
 
-          setSummary({
+          setSummary1({
             patientCount,
             averageAge,
             mostCommonCondition,
@@ -96,6 +113,253 @@ const Dashboard = () => {
     }
   }, [user]);
 
+  useEffect(() => {
+    if (patients.length > 0) {
+
+      // Environmental Factors Summary
+      const airPollutionCount = patients.filter((patient) => patient.air_pollution >= 4).length;
+      const dustExposureCount = patients.filter((patient) => patient.dust_exposure >= 4).length;
+      const geneticRiskCount = patients.filter((patient) => patient.genetic_risk >= 4).length;
+
+      // Lifestyle Factors Summary
+      const alcoholConsumptionCount = patients.filter((patient) => patient.alcohol_consumption >= 4).length;
+      const smokerCount = patients.filter((patient) => patient.smoker >= 1).length;
+      const dietComplianceCount = patients.filter((patient) => patient.balanced_diet >= 4).length;
+
+      // Respiratory Symptoms Summary
+      const coughingBloodCount = patients.filter((patient) => patient.coughing_blood >= 4).length;
+      const fatigueCount = patients.filter((patient) => patient.fatigue >= 4).length;
+      const wheezingCount = patients.filter((patient) => patient.wheezing >= 4).length;
+
+      // Chronic Conditions Overview
+      const obesityCount = patients.filter((patient) => patient.obesity >= 4).length;
+      const clubbingNailsCount = patients.filter((patient) => patient.clubbing_nails >= 4).length;
+
+      // Sleep Patterns
+      const snoringCount = patients.filter((patient) => patient.snore >= 1).length;
+
+      setSummary({
+        airPollutionCount,
+        dustExposureCount,
+        geneticRiskCount,
+        alcoholConsumptionCount,
+        smokerCount,
+        dietComplianceCount,
+        coughingBloodCount,
+        fatigueCount,
+        wheezingCount,
+        obesityCount,
+        clubbingNailsCount,
+        snoringCount,
+      });
+    }
+  }, [patients]);
+
+  useEffect(() => {
+    if (patients.length > 0) {
+      const patientLabels = patients.map((patient) => patient.name);
+      const alcoholConsumptionData = patients.map((patient) => patient.alcohol_consumption);
+      const smokerData = patients.map((patient) => patient.smoker);
+      const passiveSmokerData = patients.map((patient) => patient.passive_smoker);
+      const balancedDietData = patients.map((patient) => patient.balanced_diet);
+  
+      const lifestyleFactorsChartData = {
+        labels: patientLabels,
+        datasets: [
+          {
+            label: 'Alcohol Consumption',
+            data: alcoholConsumptionData,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+          },
+          {
+            label: 'Smoker',
+            data: smokerData,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+          },
+          {
+            label: 'Passive Smoker',
+            data: passiveSmokerData,
+            backgroundColor: 'rgba(255, 205, 86, 0.2)',
+            borderColor: 'rgba(255, 205, 86, 1)',
+            borderWidth: 1,
+          },
+          {
+            label: 'Balanced Diet',
+            data: balancedDietData,
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+          },
+        ],
+      };
+  
+      if (lifestyleFactorsChartRef.current && lifestyleFactorsChartData) {
+        const lifestyleFactorsCtx = lifestyleFactorsChartRef.current.getContext('2d');
+        new Chart(lifestyleFactorsCtx, {
+          type: 'bar',
+          data: lifestyleFactorsChartData,
+        });
+      }
+    }
+  }, [patients]);
+  
+  useEffect(() => {
+    if (patients.length > 0) {
+      const patientLabels = patients.map((patient) => patient.name);
+      const coughingBloodData = patients.map((patient) => patient.coughing_blood);
+      const fatigueData = patients.map((patient) => patient.fatigue);
+      const wheezingData = patients.map((patient) => patient.wheezing);
+  
+      const respiratorySymptomsChartData = {
+        labels: patientLabels,
+        datasets: [
+          {
+            label: 'Coughing Blood',
+            data: coughingBloodData,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          },
+          {
+            label: 'Fatigue',
+            data: fatigueData,
+            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          },
+          {
+            label: 'Wheezing',
+            data: wheezingData,
+            borderColor: 'rgba(255, 205, 86, 1)',
+            backgroundColor: 'rgba(255, 205, 86, 0.2)',
+          },
+        ],
+      };
+  
+      if (respiratorySymptomsChartRef.current && respiratorySymptomsChartData) {
+        const respiratorySymptomsCtx = respiratorySymptomsChartRef.current.getContext('2d');
+        new Chart(respiratorySymptomsCtx, {
+          type: 'radar',
+          data: respiratorySymptomsChartData,
+          options: {
+            scales: {
+              r: {
+                beginAtZero: true,
+                max: 8,
+                stepSize: 1,
+              },
+            },
+          },
+        });
+      }
+    }
+  }, [patients]);
+
+  useEffect(() => {
+    if (patients.length > 0) {
+      const patientLabels = patients.map((patient) => patient.name);
+      const airPollutionData = patients.map((patient) => patient.air_pollution);
+      const dustExposureData = patients.map((patient) => patient.dust_exposure);
+  
+      const environmentalFactorsChartData = {
+        labels: patientLabels,
+        datasets: [
+          {
+            label: 'Air Pollution',
+            data: airPollutionData,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          },
+          {
+            label: 'Dust Exposure',
+            data: dustExposureData,
+            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          },
+        ],
+      };
+  
+      if (environmentalFactorsChartRef.current && environmentalFactorsChartData) {
+        const environmentalFactorsCtx = environmentalFactorsChartRef.current.getContext('2d');
+        new Chart(environmentalFactorsCtx, {
+          type: 'radar',
+          data: environmentalFactorsChartData,
+          options: {
+            scales: {
+              r: {
+                beginAtZero: true,
+                max: 8, // Adjust the max value as needed
+                stepSize: 1, // Adjust the step size as needed
+              },
+            },
+          },
+        });
+      }
+    }
+  }, [patients]);
+  
+  useEffect(() => {
+    if (patients.length > 0) {
+      const snoringData = patients.map((patient) => patient.snore);
+      console.log(snoringData);
+      const snoringCount = snoringData.filter((snore) => snore === 1).length;
+      const nonSnoringCount = snoringData.filter((snore) => snore !== 1).length;
+
+      const snoringChartData = {
+        labels: ['Snoring', 'Not Snoring'],
+        datasets: [
+          {
+            label: 'Sleep Patterns',
+            data: [snoringCount, nonSnoringCount],
+            backgroundColor: ['#36A2EB', '#FF6384'],
+          },
+        ],
+      };
+
+      if (snoringChartRef.current && snoringChartData) {
+        const snoringCtx = snoringChartRef.current.getContext('2d');
+        new Chart(snoringCtx, {
+          type: 'bar',
+          data: snoringChartData,
+        });
+      }
+
+      
+    }
+  }, [patients]);
+
+  useEffect(() => {
+    if (patients.length > 0) {
+      const adheringToDiet = patients.filter((patient) => patient.balanced_diet <= 3).length;
+      const notAdheringToDiet = patients.length - adheringToDiet;
+  
+      const dietDonutData = {
+        labels: ['Adhering to Diet', 'Not Adhering to Diet'],
+        datasets: [
+          {
+            data: [adheringToDiet, notAdheringToDiet],
+            backgroundColor: ['#36A2EB', '#FFCE56'],
+          },
+        ],
+      };
+  
+      if (dietDonutChartRef.current && dietDonutData) {
+        const dietDonutCtx = dietDonutChartRef.current.getContext('2d');
+        
+        new Chart(dietDonutCtx, {
+          type: 'doughnut',
+          data: dietDonutData,
+          options: {
+            cutout: '80%',
+            // You can customize other options like tooltips, legend, etc.
+          },
+        });
+      }
+    }
+  }, [patients]);
+  
 
   useEffect(() => {
     if (patients.length > 0) {
@@ -122,8 +386,8 @@ const Dashboard = () => {
 
 
       // Count the number of male and female patients
-      const maleCount = genderData.filter((gender) => gender === 'male').length;
-      const femaleCount = genderData.filter((gender) => gender === 'female').length;
+      const maleCount = genderData.filter((gender) => gender === 'Male').length;
+      const femaleCount = genderData.filter((gender) => gender === 'Female').length;
 
 
       // Update risk chart data
@@ -210,10 +474,68 @@ const Dashboard = () => {
 
       <div>
         <h2>Key Metrics</h2>
-        <p>Number of Patients: {summary.patientCount}</p>
-        <p>Average Age: {summary.averageAge}</p>
-        <p>Most Common Health Condition: {summary.mostCommonCondition}</p>
+        <table>
+          <tbody>
+            <tr>
+              <td>Patients:</td>
+              <td>{summary1.patientCount}</td>
+            </tr>
+            <tr>
+              <td>Average Age:</td>
+              <td>{summary1.averageAge}</td>
+            </tr>
+            <tr>
+              <td>Air Pollution Exposure:</td>
+              <td>{summary.airPollutionCount}</td>
+            </tr>
+            <tr>
+              <td>Dust Exposure:</td>
+              <td>{summary.dustExposureCount}</td>
+            </tr>
+            <tr>
+              <td>Genetic Risks:</td>
+              <td>{summary.geneticRiskCount}</td>
+            </tr>
+            <tr>
+              <td>High Alcohol Consumption:</td>
+              <td>{summary.alcoholConsumptionCount}</td>
+            </tr>
+            <tr>
+              <td>Smokers:</td>
+              <td>{summary.smokerCount}</td>
+            </tr>
+            <tr>
+              <td>Unhealthy Diets:</td>
+              <td>{summary.dietComplianceCount}</td>
+            </tr>
+            <tr>
+              <td>Coughing Blood:</td>
+              <td>{summary.coughingBloodCount}</td>
+            </tr>
+            <tr>
+              <td>High Fatigue:</td>
+              <td>{summary.fatigueCount}</td>
+            </tr>
+            <tr>
+              <td>Wheezing:</td>
+              <td>{summary.wheezingCount}</td>
+            </tr>
+            <tr>
+              <td>Obese:</td>
+              <td>{summary.obesityCount}</td>
+            </tr>
+            <tr>
+              <td>Clubbing Nails:</td>
+              <td>{summary.clubbingNailsCount}</td>
+            </tr>
+            <tr>
+              <td>Snorer:</td>
+              <td>{summary.snoringCount}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
+
 
 
       <div>
@@ -232,6 +554,33 @@ const Dashboard = () => {
         <h2>Gender Distribution</h2>
         <canvas ref={genderChartRef} id="genderChart"></canvas>
       </div>
+
+      <div>
+        <h2>Lifestyle Factors</h2>
+        <canvas ref={lifestyleFactorsChartRef} id="lifestyleFactorsChart"></canvas>
+      </div>
+
+      <div>
+        <h2>Respiratory Symptoms</h2>
+        <canvas ref={respiratorySymptomsChartRef} id="respiratorySymptomsChart"></canvas>
+      </div>
+
+      <div>
+        <h2>Environmental Factors</h2>
+        <canvas ref={environmentalFactorsChartRef} id="environmentalFactorsChart"></canvas>
+      </div>
+
+      <div>
+        <h2>Sleep Patterns</h2>
+        <canvas ref={snoringChartRef} id="snoringChart"></canvas>
+      </div>
+
+      <div>
+        <h2>Diet Compliance Donut Chart</h2>
+        <canvas ref={dietDonutChartRef} id="dietDonutChart"></canvas>
+      </div>
+
+
     </div>
   );
 }
