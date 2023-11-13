ced826f9f412f3d27d80ffb602d212a73d6d11c4
JamieRoche1997 <144720306+JamieRoche1997@users.noreply.github.com>
2023-11-13 21:18:19 +0000

    Updates workers

--- api test.py
+++ api test.py
@@ -1,7 +1,7 @@
 import requests
 
 # Define the URL of your Flask API
-url = 'http://127.0.0.1:5000/predict'  # Replace with the actual URL
+url = 'https://healthiai-predict.onrender.com/predict'  # Replace with the actual URL
 
 # Define the JSON data you want to send
 data = {"data": [[27,2,3,1,4,2,3,2,3,3,2,2,4,2,2,2,3,4,1,5,2,6,2]]}
