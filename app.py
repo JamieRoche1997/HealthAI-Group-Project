7575e2499bf38ebb06ab6d8f1a32e04376fc9622
JamieRoche1997 <144720306+JamieRoche1997@users.noreply.github.com>
2023-11-15 00:48:36 +0000

    Updated the columns in the app.py file

--- app.py
+++ app.py
@@ -14,13 +14,7 @@ model_breast = joblib.load('model_breast.joblib')
 # Sample feature names for breast cancer model, replace this with the correct order
 feature_names_breast = ['radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean',
                         'smoothness_mean', 'compactness_mean', 'concavity_mean',
-                        'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
-                        'radius_se', 'texture_se', 'perimeter_se', 'area_se',
-                        'smoothness_se', 'compactness_se', 'concavity_se',
-                        'concave points_se', 'symmetry_se', 'fractal_dimension_se',
-                        'radius_worst', 'texture_worst', 'perimeter_worst', 'area_worst',
-                        'smoothness_worst', 'compactness_worst', 'concavity_worst',
-                        'concave points_worst', 'symmetry_worst', 'fractal_dimension_worst']
+                        'concave_points_mean']
 
 @app.route('/')
 def home():
